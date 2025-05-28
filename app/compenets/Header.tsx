2a98d56d63727491b2f0ede82a3caa576698fe9e
saed abbas <saedabbas0@gmail.com>
2025-05-28 16:48:35 +0300

    first commit

---
+++ app/compenets/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="bg-amber-600">
+        header
+      </div>
+    </div>
+  )
+}
+
+export default Header
