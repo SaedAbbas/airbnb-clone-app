15bf23d4ef92b9fe4fa73a5deafcea844109e5e8
saed abbas <saedabbas0@gmail.com>
2025-06-04 14:06:41 +0300

    new commit

--- app/compenets/header/Searchbar.tsx
+++ app/compenets/header/Searchbar.tsx
@@ -1,30 +1,70 @@
-'use client';
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker, RangeKeyDict } from "react-date-range";
+import { FaUserAlt } from "react-icons/fa";
 
 const Searchbar = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numOfGuests, setNumOfGuests] = useState(1);
 
-  const [inputValue, setInputValue] = React.useState("");
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges: RangeKeyDict) => {
+    setStartDate(ranges.selection.startDate as Date);
+    setEndDate(ranges.selection.endDate as Date);
+  };
 
   return (
     <>
-    <div className="flex items-center bg-white md:shadow-sm border border-gray-300 rounded-full px-4 py-2 max-w-3xl">
-      <input
-        type="text"
-        value={inputValue}
-        onChange ={e => setInputValue(e.target.value)}
-        placeholder="Start your search"
-        className="text-sm text-gray-600 placeholder-gray-400 flex-grow pl-5 bg-transparent outline-none"
-      />
-      <IoSearch className="bg-red-400 text-white rounded-full w-6 h-6 p-1" />
-    </div>
-    {inputValue && 
-      <div className="absolute top-16 left-1/2 transform -translate-x-1/2 bg-white shadow-lg rounded-lg p-4 w-full max-w-md">
-     saed
-      </div> 
-      }
+      <div className="flex items-center bg-white md:shadow-sm border border-gray-300 rounded-full px-4 py-2 max-w-3xl">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Start your search"
+          className="text-sm text-gray-600 placeholder-gray-400 flex-grow pl-5 bg-transparent outline-none"
+        />
+        <IoSearch className="bg-red-400 text-white rounded-full w-6 h-6 p-1" />
+      </div>
+      {inputValue && (
+        <>
+          <div className="absolute top-[61px] left-1/2 transform -translate-x-1/2 ">
+            <DateRangePicker
+              onChange={handleSelect}
+              moveRangeOnFirstSelection={false}
+              ranges={[selectionRange]}
+              className="bg-white  p-4"
+              rangeColors={["#FD5B61"]}
+              minDate={new Date()}
+            />
+            <div className="flex items-center border-t-1 justify-between bg-white rounded-b-lg p-4">
+              <label htmlFor="guests" className="text-xl ml-6 text-gray-600 font-semibold">
+                Number of Guests
+              </label>
+              <div className="flex items-center gap-x-2">
+                <FaUserAlt className="text-red-400" />
+                <input
+                  type="number"
+                  min={1}
+                  defaultValue={1}
+                  value={numOfGuests}
+                  onChange={(e) => setNumOfGuests(Number(e.target.value))}
+                  className="w-12 text-center text-red-400 border border-gray-300 rounded-md mx-2"
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
