711a408f514cffae1fdbe67868a2e3b2602b745a
saed abbas <saedabbas0@gmail.com>
2025-06-05 13:36:46 +0300

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 import Banner from "./compenets/Banner";
 import Explore from "./compenets/Explore";
+import Footer from "./compenets/Footer";
 import GreatestOutdoor from "./compenets/GreatestOutdoor";
+import Header from "./compenets/header/Header";
 import Live from "./compenets/Live";
 
 export default function Home() {
   return (
-    <div className="">
-      <Banner />
-      <Explore />
-      <Live />
-      <GreatestOutdoor 
-      img='/door.jpg'
-      title='The Greatest Outdoor'
-      description='Wishlists created by Airbnb users to inspire their next trip.'
-      linkText='Get Inspired'
-      />
-    </div>
+    <>
+      <Header />
+      <main className="">
+        <Banner />
+        <Explore />
+        <Live />
+        <GreatestOutdoor
+          img="/door.jpg"
+          title="The Greatest Outdoor"
+          description="Wishlists created by Airbnb users to inspire their next trip."
+          linkText="Get Inspired"
+        />
+      </main>
+      <Footer />
+    </>
   );
 }
