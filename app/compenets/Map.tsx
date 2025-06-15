4bb9226e88539cd411e84983d0e39f5d033443aa
saed abbas <saedabbas0@gmail.com>
2025-06-15 13:39:29 +0300

    new commit

--- app/compenets/Map.tsx
+++ app/compenets/Map.tsx
@@ -1,46 +1,44 @@
-'use client';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { SearchResultData } from '../types/app';
+"use client";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { SearchResultData } from "../types/app";
+
 
 const Map = ({ searchResultData }: { searchResultData: SearchResultData }) => {
-  const position:[number,number] = [
+  const position: [number, number] = [
     searchResultData[0].lat || 30.0444,
     searchResultData[0].long || 31.2357,
   ]; // Default to Cairo
 
-  // fix default marker icon (leaflet bug with webpack)
+  // fix default marker icon (leaflet bug with webpack) // الويب باك بعرفش يجيب صورة الايقون من النود موديولز فالكود\ هاد بحل المشكلة
   const defaultIcon = L.icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    iconUrl: '/marker.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
   });
   L.Marker.prototype.options.icon = defaultIcon;
 
   return (
-<div className='w-full h-full rounded-xl overflow-hidden'>
-  <MapContainer
-    center={position}
-    zoom={13}
-    scrollWheelZoom={true}
-    className='w-full h-full'
-  >
-    <TileLayer
-      url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    />
-    {searchResultData.map((listing) => (
-      <Marker
-        key={listing.long}
-        position={[listing.lat, listing.long]}
+    <div className="w-full h-full rounded-xl overflow-hidden">
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="w-full h-full"
       >
-        <Popup>{listing.title}</Popup>
-      </Marker>
-    ))}
-  </MapContainer>
-</div>
-
+        {/* هاي الخريطة نفسها */}
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        />
+        {searchResultData.map((listing) => (
+          <Marker key={listing.long} position={[listing.lat, listing.long]}>
+            <Popup>{listing.title}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 };
 
